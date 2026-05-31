7a29142975d8f7acbc8565ff8ed08b8f15425f91
doppelhub <buhtig.elt@xoxy.net>
2026-05-31 04:11:30 -0400

    Added E-tachometer debug text

--- Hall Sensor Tester/HallSensorTester_firmware/HallSensorTester_firmware.ino
+++ Hall Sensor Tester/HallSensorTester_firmware/HallSensorTester_firmware.ino
@@ -4,7 +4,7 @@
 
 #include "hst.h"
 
-//To update firmware:
+//To update firmware (if using Nano with LiControl bootloader):
 	//1 : Apply short from A3 to GND (2p header on PCB)
 	//2 : Upload firmware
 	//3 : Remove short (328p not boot with jumper installed)
@@ -24,16 +24,16 @@ uint8_t getHallState(void)
 
 uint8_t getHallIndex(uint8_t state)
 {
-	uint8_t stateOrder[] = { 0b001, 0b101, 0b100, 0b110, 0b010, 0b011 };
-
-	uint8_t numArrayElements = uint8_t(sizeof(stateOrder) / sizeof(stateOrder[0]));
+	static const uint8_t hallSequence[] = { 0b001, 0b101, 0b100, 0b110, 0b010, 0b011 };
+	static const uint8_t numArrayElements = uint8_t(sizeof(hallSequence) / sizeof(hallSequence[0]));
 
 	for (uint8_t ii=0; ii<numArrayElements; ii++)
 	{
-		if (stateOrder[ii] == state) { return ii; }
+		if (hallSequence[ii] == state) { return ii; }
 	}
 
-	Serial.print(F("\nError: index not found"));		
+	Serial.print(F("\nError: index not found"));
+	return 127;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -59,7 +59,10 @@ void printHallState_binary(uint8_t state)
 {
 	const uint8_t binaryDigitsToPrint = 3;
 
-	for (int8_t ii=(binaryDigitsToPrint-1); ii>=0; --ii) { Serial.print((state>>ii) & 1); }
+	for (int8_t ii=(binaryDigitsToPrint-1); ii>=0; --ii)
+	{
+		Serial.print((state>>ii) & 1);
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -69,9 +72,16 @@ void setup()
 	Serial.begin(115200); //USB
 	Serial.print(F("\n\nHall Sensor Tester v" FW_VERSION ", " BUILD_DATE "\nKEY MUST BE OFF FOR SAFETY!"));
 
-	Serial.print(F("\n\nInitial Hall State :"));
+	Serial.print(F("\nInitial Hall State :"));
 	printHallState_binary(getHallState());
-	Serial.print(F("\n\nRotate crankshaft by hand to measure Hall transitions"));
+
+    Serial.print(F("\n"
+    	"\nRotate crankshaft by hand at least one complete mechanical revolution"
+    	"\nyou should see QTY48 Hall transitions per mechanical revolution"
+    	"\n'1r' should print every QTY6 state changes (one electrical revolution)"
+    	"\n'1r' should print QTY8 times per mechanical revolution"
+        ));
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -80,6 +90,7 @@ void loop()
 {
 	uint8_t stateNow = getHallState();
 	static uint8_t previousState = stateNow;
+	static uint8_t powerupState = stateNow;
 
 	if (stateNow != previousState)
 	{
@@ -88,6 +99,8 @@ void loop()
 
 		if (isStateTransitionValid(stateNow, previousState)) { Serial.print(F(" OK"   )); }
 		else                                                 { Serial.print(F(" Invalid!")); }
+
+		if (stateNow == powerupState) { Serial.print(F(" 1r")); }
 
 		previousState = stateNow;
 	}
